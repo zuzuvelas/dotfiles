3bc12abd8115dccd01748e8270e3a5c5c04a255e
Jordon Shields <jordon.shields@flexcutech.com>
2026-07-15 09:26:36 -0600

    fix: drop `right_format` from prompt in jediterm

--- home/dot_config/exact_zsh/prompt.zsh
+++ home/dot_config/exact_zsh/prompt.zsh
@@ -1,2 +1,13 @@
 # Starship cross-shell prompt, Zuppuccin theme
+
+# Some terminals miscalculate Nerd Font column widths, which leads
+# to the cursor positioning to land in the wrong spot. Remove the
+# `right_format` line in these cases.
+if [[ "$TERMINAL_EMULATOR" == "JetBrains-JediTerm" ]]; then
+  jetbrains_config="${TMPDIR:-/tmp}/starship-jetbrains.toml"
+  grep -v '^right_format' "$STARSHIP_CONFIG" >| "$jetbrains_config"
+  export STARSHIP_CONFIG="$jetbrains_config"
+  unset jetbrains_config
+fi
+
 eval "$(starship init zsh)"
